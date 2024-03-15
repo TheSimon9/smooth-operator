c9c73bd3ab79a1502bac8263805dcf7b9199c9f6
des <christian.desimone@intre.it>
2024-03-15 17:15:01 +0100

    add finalizer implementation for human resource

--- SmoothOperator/Entities/FinalizerOne.cs
+++ SmoothOperator/Entities/FinalizerOne.cs
@@ -1,14 +1,21 @@
+using k8s.Models;
 using KubeOps.Abstractions.Finalizer;
+using KubeOps.KubernetesClient;
 using Microsoft.Extensions.Logging;
 
 namespace SmoothOperator;
 
-public class FinalizerOne(ILogger<HumanController> logger) : IEntityFinalizer<HumanEntity>
+public class FinalizerOne(ILogger<HumanController> logger, IKubernetesClient client) : IEntityFinalizer<HumanEntity>
 {
     // Called when entity is marked as deleted
     public Task FinalizeAsync(HumanEntity entity)
     {
         logger.LogInformation("Deleting an amazing human: {Entity}.", entity);
+
+        entity.Status.Status = "Deleting";
+        entity = client.UpdateStatus(entity);
+        
+        client.Delete<V1Pod>(entity.Metadata.Name, entity.Metadata.NamespaceProperty);
         return Task.CompletedTask;
     }
 }
